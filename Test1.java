df0d404d21984327df04b26664d06b8b61a2d897
kimjuock <bihwaness404@gmail.com>
2024-11-09 01:21:44 +0900

    100세건강

--- Test1.java
+++ Test1.java
@@ -1,5 +1,7 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        int a=100;
+        System.out.println(a+"살까지 사는 방법");
     }
 }
